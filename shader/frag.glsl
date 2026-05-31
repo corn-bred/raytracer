4b99ab162dab42d593ce42e21088756134de5895
corn-bred <fakter6@hotmail.com>
2026-05-31 15:34:21 -0400

    Quadratics simplification

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -20,21 +20,21 @@ class Ray {
 float willIntersectSphere(vec3 center, double radius, Ray ray) {
     vec3 OriginToCenter = center - ray.Origin;
     // Q u a d r a t i c
-    double a = dot(ray.Direction, ray.Direction); // Should be 1
-    double b = -2.0 * dot(ray.Direction, OriginToCenter); // 2(dir*oc)
-    double c = dot(OriginToCenter, OriginToCenter) - radius * radius; // oc*oc - r^2
-    double discriminant = b*b - 4*(a*c);
+    double a = pow(length(ray.Direction), 2); // Should be 1
+    double h = dot(ray.Direction, OriginToCenter); // dir*oc
+    double c = pow(length(OriginToCenter), 2) - radius * radius; // length(oc)^2 - r^2
+    double discriminant = h*h - a*c;
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return float(-b - sqrt(discriminant) ) / float(2.0*a);
+        return float(h - sqrt(discriminant) ) / float(a);
     }
 }
 
 vec3 rayColour(Ray r) {
     float t = willIntersectSphere(vec3(0,0,-1), 0.5, r);
 
-    if (t != -1.0) {
+    if (t > 0.0) {
         vec3 Normal = normalize(r.At(t) -  vec3(0.0, 0.0, -1.0));
         return (0.5 * vec3(Normal.x +1.0, Normal.y +1.0, Normal.z +1.0));
     }
