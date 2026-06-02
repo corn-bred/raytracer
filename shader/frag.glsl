8e24121afbed8e87d729b4bf99cea097760f4b27
corn-bred <fakter6@hotmail.com>
2026-06-01 21:05:09 -0400

    Added sphere class

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -8,6 +8,12 @@ uniform mat4 invProjection;
 uniform mat4 invView;
 uniform vec3 CameraPos;
 
+class HitRecord {
+    vec3 Position;
+    vec3 Normal;
+    float t;
+}
+
 class Ray {
     vec3 Origin;
     vec3 Direction;
@@ -17,18 +23,39 @@ class Ray {
     }
 };
 
-float willIntersectSphere(vec3 center, double radius, Ray ray) {
-    vec3 OriginToCenter = center - ray.Origin;
-    // Q u a d r a t i c
-    double a = pow(length(ray.Direction), 2); // Should be 1
-    double h = dot(ray.Direction, OriginToCenter); // dir*oc
-    double c = pow(length(OriginToCenter), 2) - radius * radius; // length(oc)^2 - r^2
-    double discriminant = h*h - a*c;
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return float(h - sqrt(discriminant) ) / float(a);
+class Sphere {
+    Sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {}
+
+    bool hit(Ray r, double ray_tmin, double ray_tmax, inout HitRecord rec) {
+        vec3 OriginToCenter = center - ray.Origin;
+        // Q u a d r a t i c
+        double a = pow(length(ray.Direction), 2); // Should be 1
+        double h = dot(ray.Direction, OriginToCenter); // dir*oc
+        double c = pow(length(OriginToCenter), 2) - radius * radius; // length(oc)^2 - r^2
+        double discriminant = h*h - a*c;
+
+        if (discriminant < 0)
+            return false;
+
+        double sqrtd = sqrt(discriminant);
+
+        // Find nearest root
+        double root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.Position = r.At(rec.t);
+        rec.Normal = (rec.Position - center) / radius;
+
+        return true;
     }
+
+    vec3 center;
+    double radius;
 }
 
 vec3 rayColour(Ray r) {
