092dc515d38439079f42570e617f341f6750e559
corn-bred <fakter6@hotmail.com>
2026-09-09 21:08:57 -0400

    Update .gitignore

--- build/CMakeFiles/fc-tmp/imgui/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for imgui")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/Work/Comp Projects/raytracer/build/CMakeFiles/fc-tmp/imgui/imgui-gitupdate.cmake")
-
-endblock()
